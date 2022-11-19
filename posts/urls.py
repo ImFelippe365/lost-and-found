bdac7d31f033a12997ba69e5d8f2c59b6fa21e8e
Felippe <felipperian365@gmail.com>
2022-11-19 12:00:36 -0300

    create missing pages and finish styles

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('itens', views.itens, name='itens'),
+    path('items', views.items, name='items'),
+    path('delivered-items', views.deliveredItems, name='delivered-items'),
+    path('expired-items', views.expiredItems, name='expired-items'),
+    path('all-registers', views.allRegisters, name='all-registers'),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,30 @@ def index(request):
 
     return render(request, 'index.html')
 
-def itens(request):
+def items(request):
+    context = {
+        'activeTab': 'items'
+    }
 
-    return render(request, 'itens.html')
+    return render(request, 'items.html', context)
+
+def deliveredItems(request):
+    context = {
+        'activeTab': 'delivered-items'
+    }
+
+    return render(request, 'delivered-items.html', context)
+
+def expiredItems(request):
+    context = {
+        'activeTab': 'expired-items'
+    }
+
+    return render(request, 'expired-items.html', context)
+
+def allRegisters(request):
+    context = {
+        'activeTab': 'all-registers'
+    }
+
+    return render(request, 'all-registers.html', context)
