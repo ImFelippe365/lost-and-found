90a4ff7e6866f06a148243f235fa4d79d0a9268f
Felippe <felipperian365@gmail.com>
2022-11-20 00:43:45 -0300

    new/create login page

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('login', views.login),
     path('items/', views.items, name='items'),
     path('delivered-items', views.deliveredItems, name='delivered-items'),
     path('expired-items', views.expiredItems, name='expired-items'),
--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,11 @@ def index(request):
 
     return render(request, 'index.html')
 
+def login(request):
+
+    return render(request, 'login.html')
+
+
 def items(request):
     context = {
         'activeTab': 'items',
