9dad0070f8b074c11d679898bfbbfc9871fa3093
Bruna Maria <bruna.mariap@outlook.com>
2022-11-27 22:26:00 -0300

    page update details and some changes

--- registers/views.py
+++ registers/views.py
@@ -10,6 +10,46 @@ def allRegisters(request):
     return render(request, 'all-registers.html', context)
 
 def details(request):
-    context = { }
+    context = {
+        'activeTab': 'all-registers',
+        'items': [
+            {
+                'id': 1,
+                'status': 'Perdido',
+                'image': 'image/jarra-de-vo.jpg',
+                'name': 'Jarra de porcelana',
+                'local_found': 'Sala 61',
+                'when_was_found': '16/10/2022',
+                'shift': 'noite',
+                'withdrawal_deadline': '23/12/2022',
+                'withdrawal_deadline': '23/12/2022',
+                'pickup_location': 'Bloco principal',
+            },
+            {
+                'id': 2,
+                'status': 'Entregue',
+                'image': 'image/jarra-de-vo.jpg',
+                'name': 'Jarra de porcelana',
+                'local_found': 'Na porta do refeitório',
+                'when_was_found': '16/10/2022',
+                'shift': 'noite',
+                'withdrawal_deadline': '23/12/2022',
+                'withdrawal_deadline': '23/12/2022',
+                'pickup_location': 'Bloco principal',
+            },
+            {
+                'id': 3,
+                'status': 'Expirado',
+                'image': 'image/jarra-de-vo.jpg',
+                'name': 'Jarra de porcelana',
+                'local_found': 'Na porta do refeitório',
+                'when_was_found': '16/10/2022',
+                'shift': 'noite',
+                'withdrawal_deadline': '23/12/2022',
+                'withdrawal_deadline': '23/12/2022',
+                'pickup_location': 'Bloco principal',
+            },
+        ]
+    }
 
     return render(request, 'details.html', context)
