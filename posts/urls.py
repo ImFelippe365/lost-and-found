0ef2cbe27a794574e30e3545b8e2f827f9131c97
Bruna Maria <bruna.mariap@outlook.com>
2022-12-04 18:05:02 -0300

    some changes

--- posts/urls.py
+++ posts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('login', views.login),
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
     path('items/', views.items, name='items'),
     path('delivered-items', views.deliveredItems, name='delivered-items'),
     path('expired-items', views.expiredItems, name='expired-items'),
