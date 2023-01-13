0c10d9f3f3e509553be6c6dff1d5dc2065d7f1bb
Felippe Rian <felipperian365@gmail.com>
2023-01-13 16:11:44 -0300

    Tentando colocar barra de pesquisa para javascript

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -1,13 +1,29 @@
 const search = document.getElementById('searchBar')
-const items = document.getElementsByClassName('item')
-const container = document.getElementById('itemsContainer')
+let url = window.location;
 
+console.log(window.location.pathname)
 search.addEventListener('change', ({ target }) => {
     const searchText = target.value;
-    const fullUrl = window.location.href
-    const link = fullUrl.includes('#') ? fullUrl.split('#')[0] + '#' + url : fullUrl + '#' + url
-    console.log(searchText)
-    console.log(window.location)
+
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+    });
+
+    if (params?.keyword) {
+        if (href.includes('keyword')) {
+            href.replace('?keyword', '&keyword')
+        }
+
+        window.location.href = url.replace(params.keyword, searchText)
+    } else {
+        console.log('SIMMMMMMM', url.href, url.pathname)
+        url.pathname = url.pathname + '/search'
+        console.log("NOVO -> ", url.href)
+
+        window.location.href = url.href.includes('order') ? url.href + '&keyword=' + searchText : url.href + '?keyword=' + searchText
+
+    }
+
 })
 
 
@@ -27,7 +43,6 @@ if (window.location.href.includes('order')) {
 
 order.addEventListener('change', ({ target }) => {
     const searchText = target.value;
-    const url = window.location.href
 
     hasOrder = url.includes('order');
 
