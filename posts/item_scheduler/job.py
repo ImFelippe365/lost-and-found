6f5aa8880fc7e4a6c40f213a74a684c205340ec8
Bruna Maria <bruna.mariap@outlook.com>
2023-01-14 20:28:19 -0300

    ajustando agendando tarefas

--- posts/item_scheduler/job.py
+++ posts/item_scheduler/job.py
@@ -5,11 +5,8 @@ from django.db import transaction
 
 def set_item_status():
     items = Item.objects.all().filter(status='Lost')
-    print('item scheduler')
     with transaction.atomic():
         for item in items:
-            print(item)
             if datetime.date.today() > item.withdrawal_deadline:
-                print('entrou')
                 item.status = 'Expired'
                 item.save()
