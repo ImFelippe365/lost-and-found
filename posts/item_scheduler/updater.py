12a36841699faddeef9eec10c66a47f7909990be
Bruna Maria <bruna.mariap@outlook.com>
2023-01-17 13:10:22 -0300

    Verificado itens

--- posts/item_scheduler/updater.py
+++ posts/item_scheduler/updater.py
@@ -4,5 +4,5 @@ from .job import set_item_status
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(set_item_status, 'interval', seconds=10, id='item_001', replace_existing=True)
+    scheduler.add_job(set_item_status, 'interval', minutes=1, id='item_001', replace_existing=True)
     scheduler.start()
