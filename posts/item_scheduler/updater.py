5cc43a92596307f22b1c1d3026d798ae401a7830
Bruna Maria <bruna.mariap@outlook.com>
2023-01-24 21:39:01 -0300

    Ajustando agendador de tarefas

--- posts/item_scheduler/updater.py
+++ posts/item_scheduler/updater.py
@@ -4,5 +4,6 @@ from .job import set_item_status
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(set_item_status, 'interval', minutes=1, id='item_001', replace_existing=True)
+    #scheduler.add_job(set_item_status, 'interval', minutes=1, id='item_001', replace_existing=True)
+    scheduler.add_job(set_item_status, 'cron', day ='*', hour=0, minute=0, id='item_001', replace_existing=True) #configuração para servidor
     scheduler.start()
